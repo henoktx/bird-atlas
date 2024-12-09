e5fad11738ce326370570dc9899b6eda3396349c
Henok Teixeira <henokteixeira@gmail.com>
2024-12-09 12:17:08 -0300

    feat: change dict return of class to camel case

--- app/model/Bird.py
+++ app/model/Bird.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+
 
 class Bird(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
     id: int
     scientific_name: str
     common_name: str
