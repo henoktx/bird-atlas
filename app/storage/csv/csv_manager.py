e9d733b0588062a29be9aaa72e82dc06861b732e
Henok Teixeira <henokteixeira@gmail.com>
2024-12-10 11:30:33 -0300

    feat: implements get method in csv manager

--- app/storage/csv/csv_manager.py
+++ app/storage/csv/csv_manager.py
@@ -5,7 +5,7 @@ from typing import Optional
 from app.model.Bird import Bird
 from app.storage.storage_manager import IStorageManager
 from app.storage.csv.csv_serializer import CSVSerializer
-
+from app.util.match_attribute import Matcher
 
 CSV_HEADERS = list(Bird.__annotations__.keys())
 
@@ -35,8 +35,34 @@ class CSVManager(IStorageManager):
     def delete(self, bird_id: int) -> None:
         pass
 
-    def get(self, search: Optional[str] = None) -> list[Bird]:
-        pass
+    def get(
+        self,
+        scientific_name: Optional[str],
+        common_name: Optional[str],
+        location: Optional[str],
+        average_size: Optional[float],
+        average_weight: Optional[float],
+        life_expectancy: Optional[int]
+    ) -> list[Bird]:
+        self.file.seek(0, 0)
+
+        csv_reader = csv.reader(self.file)
+        next(csv_reader)
+
+        birds = [
+            bird for line in csv_reader
+            if (
+                (bird := self.serializer.deserializer(line))
+                and Matcher.match_str_attribute(scientific_name, bird.scientific_name)
+                and Matcher.match_str_attribute(common_name, bird.common_name)
+                and Matcher.match_str_attribute(location, bird.location)
+                and Matcher.match_numeric_attribute(average_size, bird.average_size)
+                and Matcher.match_numeric_attribute(average_weight, bird.average_weight)
+                and Matcher.match_numeric_attribute(life_expectancy, bird.life_expectancy)
+            )
+        ]
+
+        return birds
 
     def _generate_next_id(self) -> int:
         with open(self.file_path, "r") as db:
