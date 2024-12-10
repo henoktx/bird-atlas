f567a19fa5ed574f9ea277bf7c13df7a721873fd
Henok Teixeira <henokteixeira@gmail.com>
2024-12-10 11:30:52 -0300

    feat: add get birds endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Optional, Any
 
 from fastapi import FastAPI
 
@@ -12,10 +13,26 @@ app = FastAPI()
 DATABASE_FILE = "database.csv"
 DATABASE: IStorageManager = CSVManager(DATABASE_FILE, CSVSerializer())
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.post("/", response_model=Bird, status_code=HTTPStatus.CREATED)
-async def create_bird(bird: Bird) -> dict[str, any]:
+async def create_bird(bird: Bird) -> Any:
     return DATABASE.save(bird).model_dump()
+
+@app.get("/", response_model=list[Bird])
+async def get_birds(
+    scientific_name: Optional[str] = None,
+    common_name: Optional[str] = None,
+    location: Optional[str] = None,
+    average_size: Optional[float] = None,
+    average_weight: Optional[float] = None,
+    life_expectancy: Optional[int] = None
+) -> Any:
+    birds = DATABASE.get(
+        scientific_name,
+        common_name,
+        location,
+        average_size,
+        average_weight,
+        life_expectancy
+    )
+
+    return birds
