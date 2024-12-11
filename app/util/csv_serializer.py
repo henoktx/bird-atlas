66ea2d96d6cf9811074973eaad78b29c8660b445
Henok Teixeira <henokteixeira@gmail.com>
2024-12-11 12:24:54 -0300

    fix: fix deserializer method with set none to null values

--- app/util/csv_serializer.py
+++ app/util/csv_serializer.py
@@ -8,24 +8,26 @@ from app.model.Bird import Bird
 class CSVSerializer:
     @staticmethod
     def serializer(bird: Bird) -> list[any]:
-        return [bird.id,
+        return [
                 bird.scientific_name,
                 bird.common_name,
                 bird.location,
                 bird.average_size,
                 bird.average_weight,
                 bird.life_expectancy,
-                bird.photo]
+                bird.photo
+        ]
 
     @staticmethod
     def deserializer(serialized_bird: list[any]) -> Bird:
+        serialized_bird = [None if value == "" else value for value in serialized_bird]
+
         return Bird(
-            id=serialized_bird[0],
-            scientific_name=serialized_bird[1],
-            common_name=serialized_bird[2],
-            location=serialized_bird[3],
-            average_size=serialized_bird[4],
-            average_weight=serialized_bird[5],
-            life_expectancy=serialized_bird[6],
-            photo=serialized_bird[7]
+            scientific_name=serialized_bird[0],
+            common_name=serialized_bird[1],
+            location=serialized_bird[2],
+            average_size=serialized_bird[3],
+            average_weight=serialized_bird[4],
+            life_expectancy=serialized_bird[5],
+            photo=serialized_bird[6]
         )
